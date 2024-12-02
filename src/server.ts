524e2b924df58b07a47f874fc29eef47994c059c
akash <wizardmakes@gmail.com>
2024-12-02 22:13:16 +0530

    fix

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("it works");
+});
+
 app.use(cors());
 app.use(express.json());
 
